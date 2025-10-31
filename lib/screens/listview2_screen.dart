0d558469c672a6bd2ad8705bf037affb625308bd
Pedro Bosch <pedro@andared.com>
2025-10-31 08:46:10 +0100

    ListView.separated acabado

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -13,14 +13,13 @@ class Listview2Screen extends StatelessWidget {
         title: Text('Listview Tipo 2'),
         backgroundColor: Colors.blue,
       ),
-      body: ListView(
-        children: [
-          ...options.map((e) => ListTile(
-            trailing: Icon(Icons.arrow_forward_ios_outlined),
-            title: Text(e),
-          ),).toList()
-          
-        ],
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          trailing: Icon(Icons.arrow_forward_ios_outlined),
+          title: Text(options[index]),
+        ),
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: options.length,
       )
     );
   }
