db6cfb168ff633c6135b82273438e2b0ec128236
Pedro Bosch <pedro@andared.com>
2025-10-31 09:02:47 +0100

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:fl_components/screens/alert_screen.dart';
+import 'package:fl_components/screens/card_screen.dart';
+import 'package:fl_components/screens/home_screen.dart';
+import 'package:fl_components/screens/listview1_screen.dart';
 import 'package:fl_components/screens/listview2_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +15,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Listview2Screen()
+      home: Listview2Screen(),
+      routes: {
+        'listview1': (BuildContext context) => Listview1Screen(),
+        'listview2': (BuildContext context) => Listview2Screen(),
+        'alert': (BuildContext context) => AlertScreen(),
+        'card': (BuildContext context) => CardScreen(),
+        'home': (BuildContext context) => HomeScreen(),
+      },
     );
   }
 }
