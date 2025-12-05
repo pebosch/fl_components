793d1247ff1bc78e099585145c82ef322cdae86b
Pedro Bosch <pedro@andared.com>
2025-12-05 09:55:36 +0100

    Arreglo problema Notch

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -7,14 +7,22 @@ class ListViewBuilderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          return FadeInImage(
-            placeholder: AssetImage('assets/jar-loading.gif'), 
-            image: NetworkImage('https://picsum.photos/500/600?image=$index')
-          );
-        },
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return FadeInImage(
+              width: double.infinity,
+              height: 250,
+              fit: BoxFit.cover,
+              placeholder: AssetImage('assets/jar-loading.gif'), 
+              image: NetworkImage('https://picsum.photos/500/600?image=$index')
+            );
+          },
+        ),
       ),
     );
   }
