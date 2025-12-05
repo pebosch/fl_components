04ce8b22efe61a79c71becddbe20496b39a12df9
Pedro Bosch <pedro@andared.com>
2025-12-05 09:47:31 +0100

    Codigo revisado

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ListViewBuilderScreen extends StatelessWidget {
+   
   const ListViewBuilderScreen({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: 100,
+        itemCount: 10,
         itemBuilder: (BuildContext context, int index) {
-          return SizedBox(
-            height: 300, // 👈 Tamaño fijo para que el scroll funcione en Windows/Web
-            child: FadeInImage(
-              fit: BoxFit.cover, // 👈 Hace que la imagen se ajuste al contenedor
-              placeholder: const AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage(
-                'https://picsum.photos/500/600?image=$index',
-              ),
-            ),
+          return FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'), 
+            image: NetworkImage('https://picsum.photos/500/600?image=$index')
           );
         },
       ),
