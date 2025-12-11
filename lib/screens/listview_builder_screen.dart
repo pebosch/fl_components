156c22195f1ef5a4a756a93e151c4edc2137f0fa
Pedro Bosch <pedro@andared.com>
2025-12-11 10:11:57 +0100

    Loading Duration 3 seconds

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,9 +1,61 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
    
   const ListViewBuilderScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+
+  final List<int> imagesIds = [1,2,3,4,5,6,7,8,9,10];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      print(' ${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}}');
+      if(scrollController.position.pixels >= scrollController.position.maxScrollExtent) {
+        //add5();
+        fetchData();
+      }
+    });
+  }
+
+  Future fetchData() async {
+    if ( isLoading ) return;
+
+    isLoading = true;
+    setState(() {
+      
+    });
+
+    await Future.delayed( Duration(seconds: 3) );
+    
+    add5();
+
+    isLoading = false;
+    setState(() {
+      
+    });
+
+  }
+
+  void add5 () {
+    final lastId = imagesIds.last;
+    imagesIds.addAll(
+      [1,2,3,4,5].map((e) => lastId + e)
+    );
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,14 +64,15 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          controller: scrollController,
+          itemCount: imagesIds.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
               width: double.infinity,
               height: 250,
               fit: BoxFit.cover,
               placeholder: AssetImage('assets/jar-loading.gif'), 
-              image: NetworkImage('https://picsum.photos/500/600?image=$index')
+              image: NetworkImage('https://picsum.photos/500/600?image=${ imagesIds[index] }')
             );
           },
         ),
